0d7762ece35ce33a87a62557076ab6928752cf7c
Adil Zhexenov <adilzhexenoff@gmail.com>
2025-06-04 19:22:50 +0500

    validation forms in JavaScript

--- js/script.js
+++ js/script.js
@@ -1,11 +1,45 @@
-const temp = +prompt("Введите текущую температуру:");
+// Валидация форм в JavaScript
+const usernameInput = document.querySelector("input[name='username']");
+const emailInput = document.querySelector("input[name='email']");
+const passwordInput = document.querySelector("input[name='password']");
 
-if (temp < -20) {
-  alert("На улице очень холодно. Наденьте все самое теплое!");
-} else if (temp >= -20 && temp <= 0) {
-  alert("На улице холодно. Убедитесь, что вы в теплой одежде!");
-} else if (temp > 0 && temp <= 20) {
-  alert("На улице тепло. Наденьте легкую куртку!");
-} else {
-  alert("На улице очень тепло. Оденьтесь легко!");
-}
+const usernameError = document.querySelector("#usernameError");
+const emailError = document.querySelector("#emailError");
+const passwordError = document.querySelector("#passwordError");
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let isValid = true;
+
+  const username = usernameInput.value;
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
+  if (username.length === 0) {
+    usernameError.textContent = "Введите свой никнейм";
+    isValid = false;
+  } else {
+    usernameError.textContent = "";
+  }
+
+  if (email.length === 0) {
+    emailError.textContent = "Введите свою электронную почту";
+    isValid = false;
+  } else {
+    emailError.textContent = "";
+  }
+
+  if (password.length === 0) {
+    passwordError.textContent = "Введите свой пароль";
+    isValid = false;
+  } else {
+    passwordError.textContent = "";
+  }
+
+  if (isValid) {
+    alert(`Пользователь ${username} с электронной почтой ${email} был успешно зарегистрирован!`);
+    form.reset();
+  }
+});
